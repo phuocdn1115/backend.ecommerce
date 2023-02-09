937023fab7557fad983bd3645932a23e1904932e
PhuocDN <phuocdn.1115@gmail.com>
2023-02-10 00:23:54 +0700

    fix resgister account

--- src/main/java/com/restfulapi/ecommerce/model/Customer.java
+++ src/main/java/com/restfulapi/ecommerce/model/Customer.java
@@ -48,7 +48,7 @@ public class Customer implements Serializable{
 	@Column(name ="username")
 	private String username;
 	
-	@OneToOne
+	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name ="address", referencedColumnName = "id")
 	private Address address;
 	
